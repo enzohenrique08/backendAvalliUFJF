3b521f30fe0c9b8420585d16bc287410a62f790f
Pablo Pinheiro Castro <pablo10544@hotmail.com>
2025-07-30 23:14:49 -0300

    criar usuario

--- app/login.py
+++ app/login.py
@@ -1,6 +1,6 @@
 import app
 from app.models import schema
-from flask import Flask, request, jsonify
+from flask import request, jsonify
 
 class Login:
     @staticmethod
@@ -16,3 +16,16 @@ class Login:
         else:
             print("Usuário não encontrado")
             return jsonify({'mensagem':'erro'})
+    def criarUsuario():
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        if not email or not senha:
+            return jsonify({'mensagem':'erro'})
+        usuarioExiste = schema.Usuario.query.filter_by(email=email).first()
+        if usuarioExiste:
+            return jsonify({'mensagem':'erro'})
+        tipo = 'usuario'
+        print(senha)
+        usuario = schema.Usuario(email,senha,tipo)
+        usuario.salvar()
+        return jsonify({'mensagem':'Sucesso'})
--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from app import create_app, login
 
 app = create_app()
 app.add_url_rule('/login',view_func=login.Login.autenticar,methods=['POST'])
+app.add_url_rule('/criar-usuario',view_func=login.Login.criarUsuario,methods=['POST'])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
